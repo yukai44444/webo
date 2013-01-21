99de9c1990d8069320834f4d0adf23aef5b8a383
kaizi <yukai44444@163.com>
2013-01-21 17:58:11 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,24 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.user = function(reg, res) {
+};
+
+exports.post = function(reg, res) {
+}
+
+exports.reg = function(reg, res) {
+}
+
+exports.doreg = function(reg, res) {
+}
+
+exports.login = function(reg, res) {
+}
+
+exports.dologin = function(reg, res) {
+}
+
+exports.logout = function(reg, res) {
+}
