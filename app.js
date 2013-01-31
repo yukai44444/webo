b164253a14fddfb7655e1f31226aaee160557993
kaizi <yukai44444@163.com>
2013-01-31 14:36:30 +0800

    init

--- app.js
+++ app.js
@@ -3,15 +3,21 @@
  * Module dependencies.
  */
 
-var express = require('express')
-, routes = require('./routes')
-, user = require('./routes/user')
-, http = require('http')
-, path = require('path');
+var express = require('express'),
+engine = require('ejs-locals'),
+routes = require('./routes'),
+http = require('http'),
+path = require('path');
 
 var app = express();
 
-app.configure(function(){
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
+
+var MongoStore = require('connect-mongo')(express),
+settings = require('./settings');
+
+app.configure(function() {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
@@ -19,23 +25,40 @@ app.configure(function(){
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: settings.cookieSecret,
+        store: new MongoStore({
+            db: settings.db
+        })
+    }));
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(function(req, res, next) {
+        res.locals.user = req.session.user;
+
+        var err = req.flash('error');
+        if (err.length)
+            res.locals.error = err;
+        else
+            res.locals.error = null;
+        var succ = req.flash('success');
+        if (succ.length)
+            res.locals.success = succ;
+        else
+            res.locals.success = null;
+
+        next();
+    });
+
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/u/:user', routes.user);
-app.post('post', routes.post);
-app.get('/reg', routes.reg);
-app.post('/reg', routes.doreg);
-app.get('/login', routes.login);
-app.post('/login', routes.dologin);
-app.get('/logout', routes.logout);
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
+
+routes(app);
